46ea2a70adc6eaefa0656ec8c2d8e6b31d153a98
deadsmond <01.lewicki@gmail.com>
2020-03-03 09:13:33 +0100

    algorithm bugfix

--- 1_cash_register.py
+++ 1_cash_register.py
@@ -8,6 +8,10 @@ class Cashbox:
         # Cashbox state
         self.cash_register = {}
 
+    def __repr__(self):
+        import json
+        return json.dumps(self.cash_register, sort_keys=True, indent=4)
+
     def add(self, cash_register: dict):
         for key in cash_register.keys():
             if key not in self.cash_register:
@@ -16,6 +20,13 @@ class Cashbox:
 
     def reset(self, cash_register: dict):
         self.cash_register = dict(cash_register)
+
+    def payment(self, amount: list):
+        for coin in amount:
+            if self.cash_register[str(coin)] > 0:
+                self.cash_register[str(coin)] -= 1
+            else:
+                raise ValueError('not enough money in cash register to pay for: %s from %s' % (coin, amount))
 
     def get_lists_from_dict(self) -> tuple:
         return list(self.cash_register.keys()), list(self.cash_register.values())
@@ -50,20 +61,19 @@ class Cashbox:
                 for c in range(len(coins)):
                     temp = solution[i] + [coins[c]]
                     # check if adding coin is legal:
-                    # if adding coin will not exceed amount
-                    if sum(temp) < amount:
-                        # check if adding coin will not exceed cash register possibilities
-                        if self.is_possible(temp):
+                    # check if adding coin will not exceed cash register possibilities
+                    if self.is_possible(temp):
+                        # if adding coin will not exceed amount
+                        if sum(temp) < amount:
                             # add coin to solution
                             solution.append(temp)
 
-                    elif sum(temp) == amount:
-                        # if solution was found
-                        return temp
+                        elif sum(temp) == amount:
+                            # if solution was found
+                            return temp
 
             # remove elements from previous iteration
             solution = solution[removable:]
-            removable = len(solution)
 
             # if the list is empty, return error
             if not solution:
@@ -72,22 +82,42 @@ class Cashbox:
             # remove duplicates
             solution = self.remove_duplicates_from_list(solution)
 
+            # set new removable on new list
+            removable = len(solution)
+
             print(solution)
 
 
 if __name__ == "__main__":
+    # init cashbox
     cashbox = Cashbox()
     cashbox.add({
-        '1': 4,
-        '4': 1,
+        '1': 24,
+        '4': 3,
         '9': 2
     })
-    amount_ = 17
-    print('solution of %s: %s' % (amount_, cashbox.depth_search_solution(amount_)))
 
+    # test payment
+    amount_ = 17
+    print('testing payment of %s from cashbox: %s' % (amount_, cashbox))
+    solution_ = cashbox.depth_search_solution(amount_)
+    print('solution of %s: %s' % (amount_, solution_))
+
+    # test payment on changed cashbox
+    cashbox.payment(solution_)
+    amount_ = 17
+    print('testing payment of %s from cashbox: %s' % (amount_, cashbox))
+    solution_ = cashbox.depth_search_solution(amount_)
+    print('solution of %s: %s' % (amount_, solution_))
+
+    # reset cashbox
     cashbox.reset({
         '1': 10,
         '2': 2
     })
+
+    # test wrong values
     amount_ = 18
-    print('solution of %s: %s' % (amount_, cashbox.depth_search_solution(amount_)))
+    print('testing payment of %s from cashbox: %s' % (amount_, cashbox))
+    solution_ = cashbox.depth_search_solution(amount_)
+    print('solution of %s: %s' % (amount_, solution_))
